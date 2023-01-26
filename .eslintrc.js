5c1eaa1365072a14ff91c2a42a511b0e15ae73ee
Tianzhen Lin (Tangent) <tangent@usa.net>
2023-01-26 16:01:29 -0500

    Initial package bootstrap (#2)
    ghstack-source-id: 77ad9378d9f15250460f0599df990d8b5c91be93
    Pull Request resolved: https://github.com/tangentlin/testzilla/pull/1
    
    Co-authored-by: Tangent Lin <>

---
+++ .eslintrc.js
@@ -0,0 +1,82 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'jest', 'simple-import-sort', 'import'],
+  env: {
+    node: true,
+    es6: true,
+  },
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    project: ['./tsconfig.json'],
+  },
+  overrides: [
+    {
+      files: ['*.ts'],
+    },
+  ],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:prettier/recommended',
+    'plugin:jest/recommended',
+  ],
+  rules: {
+    'prettier/prettier': 'warn',
+    '@typescript-eslint/no-use-before-define': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    'no-useless-escape': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-var-requires': 'off',
+    '@typescript-eslint/no-unsafe-return': 'off',
+    '@typescript-eslint/no-unsafe-call': 'off',
+    '@typescript-eslint/no-unsafe-member-access': 'off',
+    '@typescript-eslint/no-unsafe-assignment': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/ban-ts-comment': 'off',
+    '@typescript-eslint/no-empty-function': 'off',
+    'eslint-comments/no-unlimited-disable': 'off',
+    'eslint-comments/disable-enable-pair': 'off',
+    '@typescript-eslint/no-misused-promises': 'off',
+    'jest/expect-expect': 'off',
+    'no-empty': 'off',
+    'no-restricted-properties': [
+      'error',
+      {
+        property: 'substr',
+        message: 'Deprecated: Use .slice() instead of .substr().',
+      },
+    ],
+    'jest/valid-title': 'off',
+    '@typescript-eslint/no-unnecessary-type-assertion': 'off',
+    '@typescript-eslint/no-unsafe-argument': 'warn',
+    '@typescript-eslint/ban-types': 'off',
+    '@typescript-eslint/restrict-plus-operands': 'off',
+    '@typescript-eslint/restrict-template-expressions': 'off',
+    'jest/no-conditional-expect': 'off',
+    'jest/no-export': 'off',
+    '@typescript-eslint/no-empty-interface': 'off',
+    'jest/no-standalone-expect': [
+      'error',
+      {
+        additionalTestBlockFunctions: ['testIf', 'describeIf'],
+      },
+    ],
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
+    'import/first': 'error',
+    'import/newline-after-import': 'error',
+    'import/no-duplicates': 'error',
+  },
+  settings: {
+    jest: {
+      version: 27,
+      globalAliases: {
+        describe: 'describeIf',
+      },
+    },
+  },
+}
