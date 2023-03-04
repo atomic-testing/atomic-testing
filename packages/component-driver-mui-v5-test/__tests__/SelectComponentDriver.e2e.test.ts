bef404a810042c46cea995b51578529dfcf8224a
Tangent Lin <>
2023-03-04 10:46:35 -0500

    Refactor test organization - Streamline playwright test to use CRA locally (7/n) Make Playwright tests to use CRA locally, the helps save the step and cost of deploying the UI to S3.
    ghstack-source-id: 07768a9790d82e22797438a71f8e0f89f05af2a6
    Pull Request resolved: https://github.com/tangentlin/testzilla/pull/22

--- packages/component-driver-mui-v5-test/__tests__/SelectComponentDriver.e2e.test.ts
+++ packages/component-driver-mui-v5-test/__tests__/SelectComponentDriver.e2e.test.ts
@@ -1,18 +1,9 @@
 import { expect, test } from '@playwright/test';
-import { SelectComponentDriver } from '@testzilla/component-driver-mui-v5';
 import { createTestEngine } from '@testzilla/playwright'
-import { byDataTestId, ScenePart } from '@testzilla/core';
 import { basicSelectExampleScenePart } from '../src/examples';
 
-const testScenePart = {
-  select: {
-    locator: byDataTestId('demo-simple-select'),
-    driver: SelectComponentDriver,
-  },
-} satisfies ScenePart;
-
 test('happy path selection', async ({ page }) => {
-  await page.goto('http://testzilla-mui-v5.s3-website-us-east-1.amazonaws.com/select');
+  await page.goto('http://localhost:3000/select');
   const testEngine = createTestEngine(page, basicSelectExampleScenePart);
   const targetValue = '30';
   await testEngine.parts.select.setValue(targetValue);
