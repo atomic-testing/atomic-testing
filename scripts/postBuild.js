ce40eeea3cd984f762c4f73b33d127e92a48f1f5
Tangent Lin <>
2023-03-03 22:04:17 -0500

    Refactor test organization - Fix source map generation (3/n)
    [ghstack-poisoned]

--- scripts/postBuild.js
+++ scripts/postBuild.js
@@ -7,6 +7,42 @@ const skipPostBuildPackages = [
   'component-driver-mui-v5-playwright-test',
 ];
 
+/**
+ * Reduce sources path by one ../ given distribution folder is one level shallower than build/src folder
+ */
+function adjustFileSourceMap(filePath) {
+  if (!filePath.endsWith('.map')) {
+    return;
+  }
+  const fileContent = fs.readFileSync(filePath);
+  const sourceMapData = JSON.parse(fileContent);
+  if (sourceMapData.sources != null && sourceMapData.sources.length > 0) {
+    sourceMapData.sources = sourceMapData.sources.map((src) => {
+      if (src.startsWith('../')) {
+        return src.slice(3);
+      }
+    });
+    const adjusted = JSON.stringify(sourceMapData);
+    fs.writeFileSync(filePath, adjusted);
+  }
+}
+
+function isFolder(p) {
+  return fs.statSync(p).isDirectory();
+}
+
+function adjustFolderSourceMap(dir) {
+  const files = fs.readdirSync(dir);
+  for (const p of files) {
+    const fullPath = path.join(dir, p);
+    if (isFolder(fullPath)) {
+      adjustFolderSourceMap(fullPath);
+    } else {
+      adjustFileSourceMap(fullPath);
+    }
+  }
+}
+
 function postBuild(dir) {
   // Rename package/build/src to package/dist
   if (skipPostBuildPackages.some((pkg) => dir.includes(pkg))) {
@@ -22,6 +58,7 @@ function postBuild(dir) {
   const buildSrc = path.join(build, 'src');
 
   fs.cpSync(buildSrc, dist, { recursive: true });
+  adjustFolderSourceMap(dist);
 }
 
 postBuild(process.cwd());
