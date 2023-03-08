1b7f1f8143e9e5f2d97200c4b590ff7f40d1830a
Tangent Lin <>
2023-03-08 18:43:05 -0500

    HTML Component - remove superfluous views Contents in views folder repeats itself, this diff promotes the content to directory.tsx to save the number of files.
    [ghstack-poisoned]

--- packages/component-driver-html-test/src/views/HTMLCheckboxExample.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { ExampleList } from '../components/ExampleList';
-import { checkboxExamples } from '../examples';
-
-export const HTMLCheckboxExample: React.FunctionComponent = () => (
-  <ExampleList examples={checkboxExamples} />
-);
--- packages/component-driver-html-test/src/views/HTMLRadioButtonGroupExample.tsx
+++
@@ -1,8 +0,0 @@
-import { radioButtonGroupExamples } from '../examples/HTMLRadioButtonGroup.examples';
-import React from 'react';
-import { ExampleList } from '../components/ExampleList';
-
-export const HTMLRadioButtonGroupExample: React.FunctionComponent = () => (
-  // @ts-ignore
-  <ExampleList examples={radioButtonGroupExamples} />
-);
