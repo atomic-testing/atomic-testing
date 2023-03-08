504e4f81de372d664a0a24bea52cfb489c93b64b
Tangent Lin <>
2023-03-07 22:36:03 -0500

    Fix build issue A misspelled TextArea file inclusion caused the build to fail
    [ghstack-poisoned]

--- packages/component-driver-html/src/index.ts
+++ packages/component-driver-html/src/index.ts
@@ -3,5 +3,5 @@ export { HTMLCheckboxGroupDriver } from './components/HTMLCheckboxGroupDriver';
 export { HTMLHiddenInputDriver } from './components/HTMLHiddenInputDriver';
 export { HTMLRadioButtonGroupDriver } from './components/HTMLRadioButtonGroupDriver';
 export { HTMLSelectDriver } from './components/HTMLSelectDriver';
-export { HTMLTextAreaDriver } from './components/HTMLTextAreaDriver';
+export { HTMLTextAreaDriver } from './components/HTMLTextareaDriver';
 export { HTMLTextInputDriver } from './components/HTMLTextInputDriver';
