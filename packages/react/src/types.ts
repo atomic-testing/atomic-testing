84c6f5bd715da33b8f493b810718538bd3250f50
Tangent Lin <>
2023-03-15 21:49:31 -0400

    Support Legacy React rendering
    [ghstack-poisoned]

--- packages/react/src/types.ts
+++ packages/react/src/types.ts
@@ -2,4 +2,8 @@ import { IComponentDriverOption } from '@atomic-testing/core';
 
 export interface IReactTestEngineOption extends IComponentDriverOption {
   rootElement?: Element;
+  /**
+   * When true, it would use React 17 or before to render react component, default is false.
+   */
+  legacyRender?: boolean;
 }
