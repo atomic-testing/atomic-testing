798621a3ebe438e34085767e97d5cf0c9cdf29e9
Tangent Lin <>
2023-03-16 13:07:04 -0400

    Utility script for versioning and publishing
    [ghstack-poisoned]

---
+++ publish.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# Bulk publish all packages
+# Use inconjunction with pnpm bumpVersion #.#.# to update all package versions
+
+pnpm -r build
+
+cd packages/core
+pnpm publish --access=public
+
+cd ../dom-core
+pnpm publish --access=public
+
+cd ../playwright
+pnpm publish --access=public
+
+cd ../react
+pnpm publish --access=public
+
+cd ../component-driver-html
+pnpm publish --access=public
+
+cd ../component-driver-mui-v5
+pnpm publish --access=public
+
+cd ../../
+echo "Done!"
+
---
+++ scripts/bumpVersion.ts
@@ -0,0 +1,39 @@
+import fs from 'fs';
+import path from 'path';
+
+const skipPostBuildPackages = ['component-driver-html-test', 'component-driver-mui-v5-test'];
+
+function isFolder(p: string): boolean {
+  return fs.statSync(p).isDirectory();
+}
+
+function adjustFolderPackageJson(dir: string, version: string) {
+  const fileName = path.join(dir, 'package.json');
+  if (!fs.existsSync(fileName)) {
+    return;
+  }
+  const fileContent = fs.readFileSync(fileName).toString();
+  const newContent = fileContent.replace(/("version"\:\s+").*(")/g, `$1${version}$2`);
+  fs.writeFileSync(fileName, newContent);
+}
+
+function bumpVersion(dir: string, version: string) {
+  const santizedVersion = version.trim();
+  if (santizedVersion.length < 1) {
+    return;
+  }
+
+  const packagePath = path.join(dir, 'packages');
+  const children = fs.readdirSync(packagePath);
+  for (const child of children) {
+    if (skipPostBuildPackages.includes(child)) {
+      continue;
+    }
+    const childPath = path.join(packagePath, child);
+    if (isFolder(childPath)) {
+      adjustFolderPackageJson(childPath, santizedVersion);
+    }
+  }
+}
+
+bumpVersion(process.cwd(), process.argv[2]);
