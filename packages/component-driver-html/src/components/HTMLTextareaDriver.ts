56a980c8a30d4d2c0b9b7681b92a8c54f6a76903
tangentlin <tangent@usa.net>
2023-03-16 07:27:50 -0400

    Rename file Textarea to TextArea

--- packages/component-driver-html/src/components/HTMLTextareaDriver.ts
+++
@@ -1,7 +0,0 @@
-import { HTMLTextInputDriver } from './HTMLTextInputDriver';
-
-export class HTMLTextAreaDriver extends HTMLTextInputDriver {
-  override get driverName(): string {
-    return 'HTMLTextArea';
-  }
-}
