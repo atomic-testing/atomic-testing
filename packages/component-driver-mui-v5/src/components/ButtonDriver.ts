50be7385a56144d7b15b41db9af88eee503288c1
Tangent Lin <tangent@usa.net>
2023-03-20 11:26:09 -0400

    MUI V5 - FAB Driver (6/n)
    [ghstack-poisoned]

--- packages/component-driver-mui-v5/src/components/ButtonDriver.ts
+++ packages/component-driver-mui-v5/src/components/ButtonDriver.ts
@@ -1,5 +1,9 @@
 import { HTMLButtonDriver } from '@atomic-testing/component-driver-html';
 
+/**
+ * Driver for Material UI v5 Button component.
+ * @see https://mui.com/material-ui/react-button/
+ */
 export class ButtonDriver extends HTMLButtonDriver {
   override get driverName(): string {
     return 'MuiV5ButtonDriver';
