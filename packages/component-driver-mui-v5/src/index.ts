d2b1da9d045ede5bc6b2cbc52f7ccda5e354032a
Tangent Lin <>
2023-03-20 21:36:53 -0400

    MUI V5 - FAB Driver (6/n)
    ghstack-source-id: 1a206c9ba8a6078b0153ffa6a1ae114b86ed001e
    Pull Request resolved: https://github.com/tangentlin/atomic-testing/pull/66

--- packages/component-driver-mui-v5/src/index.ts
+++ packages/component-driver-mui-v5/src/index.ts
@@ -1,5 +1,6 @@
 export { ButtonDriver } from './components/ButtonDriver';
 export { CheckboxDriver } from './components/CheckboxDriver';
+export { FabDriver } from './components/FabDriver';
 export { InputDriver } from './components/InputDriver';
 export { SelectDriver } from './components/SelectDriver';
 export { TextFieldDriver } from './components/TextFieldDriver';
