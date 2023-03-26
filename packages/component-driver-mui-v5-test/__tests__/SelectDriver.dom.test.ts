493f4a8ae0b26bfded14461af9623a3ddc76d760
Tangent Lin <>
2023-03-26 12:29:53 -0400

    Generic test runner: Migrate MUI V5 Select (9/n)
    ghstack-source-id: 265674a7c2d6691871af10caf588f98d983f1b1d
    Pull Request resolved: https://github.com/atomic-testing/atomic-testing/pull/95

---
+++ packages/component-driver-mui-v5-test/__tests__/SelectDriver.dom.test.ts
@@ -0,0 +1,16 @@
+import { jestTestAdapter } from '@atomic-testing/jest';
+import { createTestEngine } from '@atomic-testing/react';
+import { testRunner } from '@atomic-testing/test-runner';
+import { basicSelectExample, basicSelectTestSuite, nativeSelectExample, nativeSelectTestSuite } from '../src/examples';
+
+testRunner(basicSelectTestSuite, jestTestAdapter, {
+  getTestEngine: (scenePart: typeof basicSelectExample.scene) => {
+    return createTestEngine(basicSelectExample.ui, scenePart);
+  },
+});
+
+testRunner(nativeSelectTestSuite, jestTestAdapter, {
+  getTestEngine: (scenePart: typeof nativeSelectExample.scene) => {
+    return createTestEngine(nativeSelectExample.ui, scenePart);
+  },
+});
--- packages/component-driver-mui-v5-test/__tests__/SelectDriver.e2e.test.ts
+++ packages/component-driver-mui-v5-test/__tests__/SelectDriver.e2e.test.ts
@@ -1,15 +1,7 @@
-import { createTestEngine } from '@atomic-testing/playwright';
-import { expect, test } from '@playwright/test';
-import { basicSelectExampleScenePart, selectExamples } from '../src/examples';
+import { getTestRunnerInterface, playWrightTestFrameworkMapper } from '@atomic-testing/playwright';
+import { testRunner } from '@atomic-testing/test-runner';
+import { basicSelectTestSuite, nativeSelectTestSuite } from '../src/examples';
 
-selectExamples.forEach((example) => {
-  test(`${example.title}`, async ({ page }) => {
-    await page.goto('/select');
-    const testEngine = createTestEngine(page, basicSelectExampleScenePart);
-    const targetValue = '30';
-    await testEngine.parts.select.setValue(targetValue);
-    const val = await testEngine.parts.select.getValue();
-    expect(val).toBe(targetValue);
-    await testEngine.cleanUp();
-  });
-});
+testRunner(basicSelectTestSuite, playWrightTestFrameworkMapper, getTestRunnerInterface());
+
+testRunner(nativeSelectTestSuite, playWrightTestFrameworkMapper, getTestRunnerInterface());
