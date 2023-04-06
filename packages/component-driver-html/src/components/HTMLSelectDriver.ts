37bb0e111612fbdc3f11023144696e6e4224e185
Tangent Lin <>
2023-04-06 18:52:44 -0400

    HTML Select: Add select by label
    ghstack-source-id: b41e460ba8f6c8a30c098c5d00dac0d75e3c8c09
    Pull Request resolved: https://github.com/atomic-testing/atomic-testing/pull/128

--- packages/component-driver-html/src/components/HTMLSelectDriver.ts
+++ packages/component-driver-html/src/components/HTMLSelectDriver.ts
@@ -1,11 +1,24 @@
-import { ComponentDriver, IInputDriver, Nullable } from '@atomic-testing/core';
+import {
+  ComponentDriver,
+  IInputDriver,
+  LocatorType,
+  locatorUtil,
+  Nullable,
+  PartLocatorType,
+} from '@atomic-testing/core';
+
+import { HTMLOptionDriver } from './HTMLOptionDriver';
 
 type ValueT = string | readonly string[];
 
 export class HTMLSelectDriver extends ComponentDriver<{}> implements IInputDriver<Nullable<ValueT>> {
-  async getValue(): Promise<Nullable<ValueT>> {
+  async isMultiple(): Promise<boolean> {
     const multiple = await this.interactor.getAttribute(this.locator, 'multiple');
-    const isMultiple = multiple != null;
+    return multiple != null;
+  }
+
+  async getValue(): Promise<Nullable<ValueT>> {
+    const isMultiple = await this.isMultiple();
     const values = await this.interactor.getSelectValues(this.locator);
     const returnedValue = isMultiple ? values : values?.[0];
     return returnedValue ?? null;
@@ -18,6 +31,61 @@ export class HTMLSelectDriver extends ComponentDriver<{}> implements IInputDrive
     }
     await this.interactor.selectOptionValue(this.locator, selectedValues);
     return true;
+  }
+
+  async getOptionByLocator(itemLocator: PartLocatorType): Promise<HTMLOptionDriver | null> {
+    const locator = locatorUtil.append(this.locator, itemLocator);
+    const exists = await this.interactor.exists(locator);
+    if (exists) {
+      return new HTMLOptionDriver(locator, this.interactor);
+    } else {
+      return null;
+    }
+  }
+
+  async getOptionByIndex(index: number): Promise<HTMLOptionDriver | null> {
+    const itemLocator: PartLocatorType = {
+      type: LocatorType.Css,
+      selector: `option:nth-of-type(${index + 1})`,
+    };
+    return this.getOptionByLocator(itemLocator);
+  }
+
+  async getValuesByLabels(labels: readonly string[]): Promise<readonly string[]> {
+    const labelSet = new Set(labels);
+    let index = 0;
+    let item: HTMLOptionDriver | null = await this.getOptionByIndex(index);
+    const values: string[] = [];
+    while (item != null) {
+      const label = await item.label();
+      const value = await item.value();
+      if (label != null && labelSet.has(label) && value != null) {
+        values.push(value);
+      }
+      index++;
+      item = await this.getOptionByIndex(index);
+    }
+    return values;
+  }
+
+  async selectByLabel(label: string | readonly string[]): Promise<void> {
+    const labels = Array.isArray(label) ? label : [label];
+    const values = await this.getValuesByLabels(labels);
+    await this.setValue(values);
+  }
+
+  async getSelectedLabel(isMultiple: true): Promise<readonly string[] | null>;
+  async getSelectedLabel(isMultiple: false): Promise<string | null>;
+  async getSelectedLabel(): Promise<string | null>;
+  async getSelectedLabel(isMultiple?: boolean) {
+    if (await this.exists()) {
+      const labels = await this.interactor.getSelectLabels(this.locator);
+      if (isMultiple) {
+        return labels;
+      }
+      return labels![0] ?? null;
+    }
+    return null;
   }
 
   isDisabled(): Promise<boolean> {
--- packages/component-driver-html/src/index.ts
+++ packages/component-driver-html/src/index.ts
@@ -4,6 +4,7 @@ export { HTMLCheckboxDriver } from './components/HTMLCheckboxDriver';
 export { HTMLCheckboxGroupDriver } from './components/HTMLCheckboxGroupDriver';
 export { HTMLElementDriver } from './components/HTMLElementDriver';
 export { HTMLHiddenInputDriver } from './components/HTMLHiddenInputDriver';
+export { HTMLOptionDriver } from './components/HTMLOptionDriver';
 export { HTMLRadioButtonGroupDriver } from './components/HTMLRadioButtonGroupDriver';
 export { HTMLSelectDriver } from './components/HTMLSelectDriver';
 export { HTMLTextAreaDriver } from './components/HTMLTextAreaDriver';
