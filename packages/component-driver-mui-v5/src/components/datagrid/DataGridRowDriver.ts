ffe18843b85f9b92b6c0ba090ff35974010bdc21
Tangent <tangent.lin@premise.com>
2023-05-05 14:54:43 -0400

    MUI V5: DataGridPro additional driver methods

---
+++ packages/component-driver-mui-v5/src/components/datagrid/DataGridRowDriver.ts
@@ -0,0 +1,44 @@
+import { HTMLElementDriver } from '@atomic-testing/component-driver-html';
+import { byAttribute, ComponentDriver, listHelper, locatorUtil, PartLocator } from '@atomic-testing/core';
+
+export abstract class DataGridRowDriver extends ComponentDriver {
+  protected async getCellCount(): Promise<number> {
+    let count = 0;
+    for await (const _ of listHelper.getListItemIterator(this, this.getCellLocator(), HTMLElementDriver)) {
+      count++;
+    }
+    return count;
+  }
+
+  async getRowText(): Promise<string[]> {
+    const textList: string[] = [];
+    for await (const cell of listHelper.getListItemIterator(this, this.getCellLocator(), HTMLElementDriver)) {
+      const text = await cell.getText();
+      textList.push(text!.trim());
+    }
+    return textList;
+  }
+
+  async getCell<DriverT extends ComponentDriver>(
+    cellIndexOrField: number | string, // number: column index, string: column field
+    // @ts-ignore
+    driverClass: typeof ComponentDriver = HTMLElementDriver,
+  ): Promise<DriverT | null> {
+    let cellLocator: PartLocator;
+    if (typeof cellIndexOrField === 'number') {
+      cellLocator = byAttribute('data-colindex', cellIndexOrField.toString());
+    } else {
+      cellLocator = byAttribute('data-field', cellIndexOrField);
+    }
+    const locator = locatorUtil.append(this.locator, cellLocator);
+    const cellExists = await this.interactor.exists(locator);
+    if (cellExists) {
+      // @ts-ignore
+      return new driverClass(locator, this.interactor, this.commutableOption);
+    }
+
+    return null;
+  }
+
+  protected abstract getCellLocator(): PartLocator;
+}
