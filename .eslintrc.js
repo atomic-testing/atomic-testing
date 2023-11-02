0757da4e3fc5512b223edb2e767965719715f5ca
Tangent Lin <>
2023-11-02 16:17:48 +0800

    MUI V5: Accordion driver
    Add new driver for MUI 5 accordion component

--- .eslintrc.js
+++ .eslintrc.js
@@ -82,7 +82,7 @@ module.exports = {
       },
     ],
     'simple-import-sort/imports': 'warn',
-    'simple-import-sort/exports': 'error',
+    'simple-import-sort/exports': 'off',
     'import/first': 'error',
     'import/newline-after-import': 'error',
     'import/no-duplicates': 'error',
