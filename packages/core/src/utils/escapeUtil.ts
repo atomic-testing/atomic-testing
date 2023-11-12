aab2da1ba05a302c23bfbff4f40c9be4a926083f
Tianzhen Lin (Tangent) <tangent@usa.net>
2023-11-12 13:54:27 -0500

    fix: (core) Properly escape attribute values (#193)
    Fix when constructing CSS selector, attribute values are not properly escaped

--- packages/core/src/utils/escapeUtil.ts
+++ packages/core/src/utils/escapeUtil.ts
@@ -1,15 +1,67 @@
+const cssEscapes = new Map([
+  ['!', '\\!'],
+  ['"', '\\"'],
+  ['#', '\\#'],
+  ['$', '\\$'],
+  ['%', '\\%'],
+  ['&', '\\&'],
+  ["'", "\\'"],
+  ['(', '\\('],
+  [')', '\\)'],
+  ['*', '\\*'],
+  ['+', '\\+'],
+  [',', '\\,'],
+  ['.', '\\.'],
+  ['/', '\\/'],
+  [':', '\\:'],
+  [';', '\\;'],
+  ['<', '\\<'],
+  ['=', '\\='],
+  ['>', '\\>'],
+  ['?', '\\?'],
+  ['@', '\\@'],
+  ['[', '\\['],
+  ['\\', '\\\\'],
+  [']', '\\]'],
+  ['^', '\\^'],
+  ['`', '\\`'],
+  ['{', '\\{'],
+  ['|', '\\|'],
+  ['}', '\\}'],
+  ['~', '\\~'],
+  [' ', '\\ '],
+]);
+
 export function escapeName(name: string): string {
   return encodeURIComponent(name);
 }
 
+const escapeCache = new Map();
+
+/**
+ * Escaping based on the CSS spec: https://www.w3.org/TR/CSS21/syndata.html#value-def-identifier
+ * Backslashes, spaces, and non-identifier characters (e.g., ! " # $ % & ' ( ) * + , . / : ; < = > ? @ [ ] ^ ` { | } ~) are escaped.
+ * @param value
+ * @returns
+ */
 export function escapeValue(value: string): string {
-  return encodeURIComponent(value);
+  if (escapeCache.has(value)) {
+    return escapeCache.get(value);
+  }
+
+  let escapedValue = '';
+  for (const character of value) {
+    if (cssEscapes.has(character)) {
+      escapedValue += cssEscapes.get(character);
+      continue;
+    }
+    escapedValue += character;
+  }
+
+  escapeCache.set(value, escapedValue);
+  return escapedValue;
 }
 
 export function escapeCssClassName(name: string): string {
   return name;
-}
-
-export function escapeId(id: string): string {
-  return id.split(':').join('\\:');
 }
