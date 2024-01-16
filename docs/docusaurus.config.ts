970e0477ffaf7b39987cfed578404487c4fb6194
Tangent Lin <>
2024-01-16 16:36:03 -0500

    fix: Change URL of the website to include www
    Include www so it works with search engine Algolia.
    
    See: https://discourse.algolia.com/t/crawler-skipping-my-website-for-reason-domain-not-allowed/19347

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -16,7 +16,7 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://atomic-testing.dev',
+  url: 'https://www.atomic-testing.dev',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
