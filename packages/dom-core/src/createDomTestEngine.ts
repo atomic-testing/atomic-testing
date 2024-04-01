8637992b78c1dc2740bdf06c8f96bbf3f6bcfcfc
Tianzhen Lin (Tangent) <tangent@usa.net>
2024-03-31 22:20:54 -0400

    feat: Add createDomTestEngine for pure DOM test (#209)
    Summary:
    There is no easy bootstrap function for pure DOM tests, which could be useful for Storybook interaction testing

---
+++ packages/dom-core/src/createDomTestEngine.ts
@@ -0,0 +1,27 @@
+import { ScenePart, TestEngine } from '@atomic-testing/core';
+
+import { DOMInteractor } from './DOMInteractor';
+import { IDomTestEngineOption } from './types';
+
+/**
+ * Create test engine for DOM testing
+ * @param element The element to test, if not sure, use document.body
+ * @param partDefinitions The scene part definitions
+ * @param _option The option, reserved for future use
+ * @returns The test engine
+ */
+export function createDomTestEngine<T extends ScenePart>(
+  element: HTMLElement,
+  partDefinitions: T,
+  _option?: IDomTestEngineOption,
+): TestEngine<T> {
+  const cleanup = () => Promise.resolve();
+  return new TestEngine(
+    [],
+    new DOMInteractor(element),
+    {
+      parts: partDefinitions,
+    },
+    cleanup,
+  );
+}
--- packages/dom-core/src/index.ts
+++ packages/dom-core/src/index.ts
@@ -1,2 +1,4 @@
 export { DOMInteractor } from './DOMInteractor';
+export * from './createDomTestEngine';
 export * from './fakeEvents';
+export * from './types';
