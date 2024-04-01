a422ece1b6ed96423ed743ab7ed7c49e821b9852
Tangent Lin <>
2024-04-01 02:46:33 -0400

    feat: Support html date, time and datetime-local input
    Summary:
    Support html date, time and datetime-local input, provide guard on datetime format, and throw error with details on the correct format, this would provide better developer experience by not letting tests fail silently.

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,3 +1,4 @@
+export { TestEngine } from './TestEngine';
 export type { Nullable, Optional } from './dataTypes';
 export * from './drivers';
 export * from './errors';
@@ -12,8 +13,15 @@ export type {
   ScenePartDefinition,
   ScenePartDriver,
 } from './partTypes';
-export { TestEngine } from './TestEngine';
 export * as collectionUtil from './utils/collectionUtil';
+export * as dateUtil from './utils/dateUtil';
+export type {
+  DateValidationFailureResult,
+  DateValidationResult,
+  DateValidationSuccessResult,
+  HtmlInputDateType,
+  htmlInputDateTypes,
+} from './utils/dateUtil';
 export * as domUtil from './utils/domUtil';
 export * as escapeUtil from './utils/escapeUtil';
 export * as locatorUtil from './utils/locatorUtil';
---
+++ packages/core/src/utils/__tests__/dateUtil.test.ts
@@ -0,0 +1,60 @@
+import {
+  isHtmlInputDateFormat,
+  isHtmlInputDateTimeFormat,
+  isHtmlInputTimeFormat,
+  validateHtmlDateInput,
+} from '../dateUtil';
+
+describe('isHtmlInputDateFormat', () => {
+  test.each([
+    ['2021-01-01', true],
+    ['2021-01-01T15:30', false],
+    ['15:30', false],
+    ['2021-01-32', false],
+    ['2020-02-29', true], // Leap year
+    ['2021-02-29', false], // Non leap year
+  ])('%s should validated as %p', (input, expected) => {
+    expect(isHtmlInputDateFormat(input)).toBe(expected);
+  });
+});
+
+describe('isHtmlInputTimeFormat', () => {
+  test.each([
+    ['2021-01-01', false],
+    ['2021-01-01T15:30', false],
+    ['15:30:12', false],
+    ['03:30PM', false],
+    ['15:30', true],
+    ['00:00', true],
+    ['02:30', true],
+    ['23:59', true],
+    ['23:60', false],
+    ['23:100', false],
+    ['24:01', false],
+  ])('%s should validated as %p', (input, expected) => {
+    expect(isHtmlInputTimeFormat(input)).toBe(expected);
+  });
+});
+
+describe('isHtmlInputDateTimeFormat', () => {
+  test.each([
+    ['2021-01-01', false],
+    ['2021-01-01T15:30', true],
+    ['15:30', false],
+  ])('%s should validated as %p', (input, expected) => {
+    expect(isHtmlInputDateTimeFormat(input)).toBe(expected);
+  });
+});
+
+describe('validateHtmlDateInput', () => {
+  test.each([
+    ['date', '2021-01-01', true],
+    ['datetime-local', '2021-01-01T15:30', true],
+    ['time', '15:30', true],
+    ['datetime-local', '2021-01-01', false],
+    ['date', '2021-01-01T15:30', false],
+    ['date', '15:30', false],
+  ])('type: %s with value %s should validated as %p', (type, input, expected) => {
+    expect(validateHtmlDateInput(type, input).valid).toBe(expected);
+  });
+});
