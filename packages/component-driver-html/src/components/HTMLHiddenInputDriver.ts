402b5f2d5d4842805b4c30c595e8e0098fdce067
Tangent Lin <>
2024-04-01 21:20:15 -0400

    fix: Some driver constructor should use partial option type
    Summary:
    Some drivers such as CheckboxDriver incorrectly expects full `IComponentDriverOption` type instead of partial, this causes incompatiblity when using with ListHelper.

--- packages/component-driver-html/src/components/HTMLHiddenInputDriver.ts
+++ packages/component-driver-html/src/components/HTMLHiddenInputDriver.ts
@@ -1,7 +1,7 @@
 import { ComponentDriver, IComponentDriverOption, IInputDriver, Interactor, PartLocator } from '@atomic-testing/core';
 
 export class HTMLHiddenInputDriver extends ComponentDriver<{}> implements IInputDriver<string | null> {
-  constructor(locator: PartLocator, interactor: Interactor, option?: IComponentDriverOption) {
+  constructor(locator: PartLocator, interactor: Interactor, option?: Partial<IComponentDriverOption>) {
     super(locator, interactor, {
       ...option,
       parts: {},
