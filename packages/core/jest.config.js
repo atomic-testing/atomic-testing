98c69c8ebaefe3c82561771f7dd9025e1c92c294
Tangent Lin <>
2025-03-08 09:13:19 -0500

    chore: Update Node, Playwright versions
    * Update @types/node to 22
    * Update Playwright to 1.51.2
    * Fix circular dependencies

--- packages/core/jest.config.js
+++ packages/core/jest.config.js
@@ -3,5 +3,5 @@ const base = require('../../jest.config.base.js');
 module.exports = {
   ...base,
   roots: ['<rootDir>/src'],
-  displayName: 'core',
+  displayName: 'component-driver-mui-x-v7-test',
 };
