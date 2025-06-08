6a2b0e727b99b674b38d5f1dd68ca2b523d011ca
Tangent Lin <tangent.lin@sandboxquantum.com>
2025-06-08 15:11:53 -0400

    refactor: MUI v7 tests
    * Split examples and test suite
    * Use Vite instead of React-script for build

---
+++ packages/component-driver-mui-v7-test/src/examples/accordion/BasicAccordion.suite.ts
@@ -0,0 +1,56 @@
+import { AccordionDriver } from '@atomic-testing/component-driver-mui-v7';
+import { TestEngine, byDataTestId, ScenePart, IExampleUnit } from '@atomic-testing/core';
+import { TestSuiteInfo } from '@atomic-testing/test-runner';
+
+import { basicAccordionUIExample } from './BasicAccordion.examples';
+
+export const basicAccordionExampleScenePart = {
+  normalAccordion: {
+    locator: byDataTestId('accordion-normal'),
+    driver: AccordionDriver,
+  },
+  disabledAccordion: {
+    locator: byDataTestId('accordion-disabled'),
+    driver: AccordionDriver,
+  },
+} satisfies ScenePart;
+
+export const basicAccordionExample: IExampleUnit<typeof basicAccordionExampleScenePart, JSX.Element> = {
+  ...basicAccordionUIExample,
+  scene: basicAccordionExampleScenePart,
+};
+
+export const basicAccordionTestSuite: TestSuiteInfo<typeof basicAccordionExample.scene> = {
+  title: 'Basic Accordion',
+  url: '/accordion',
+  tests: (getTestEngine, { describe, test, beforeEach, afterEach, assertEqual }) => {
+    describe(`${basicAccordionExample.title}`, () => {
+      let testEngine: TestEngine<typeof basicAccordionExample.scene>;
+
+      // Use the following beforeEach to work around the issue of Playwright's page being undefined
+      // @ts-ignore
+      beforeEach(function ({ page }) {
+        // @ts-ignore
+        testEngine = getTestEngine(basicAccordionExample.scene, { page });
+        if (typeof arguments[0] === 'function') {
+          arguments[0]();
+        }
+      });
+
+      afterEach(async () => {
+        await testEngine.cleanUp();
+      });
+
+      test(`Normal accordion should be expanded and show content when clicked`, async () => {
+        await testEngine.parts.normalAccordion.expand();
+        const expanded = await testEngine.parts.normalAccordion.isExpanded();
+        assertEqual(expanded, true);
+      });
+
+      test(`Disabled accordion is not interactive`, async () => {
+        const disabled = await testEngine.parts.disabledAccordion.isDisabled();
+        assertEqual(disabled, true);
+      });
+    });
+  },
+};
--- packages/component-driver-mui-v7-test/src/examples/button/IconAndLabelButton.example.tsx
+++ packages/component-driver-mui-v7-test/src/examples/button/IconAndLabelButton.example.tsx
@@ -1,102 +1,31 @@
 import React from 'react';
 
-import { HTMLElementDriver } from '@atomic-testing/component-driver-html';
-import { ButtonDriver } from '@atomic-testing/component-driver-mui-v6';
-import { byDataTestId, IExampleUnit, ScenePart, TestEngine } from '@atomic-testing/core';
-import { TestSuiteInfo } from '@atomic-testing/test-runner';
-import AlarmIcon from '@mui/icons-material/Alarm';
+import { IExampleUIUnit } from '@atomic-testing/core';
+import DeleteIcon from '@mui/icons-material/Delete';
 import SendIcon from '@mui/icons-material/Send';
 import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
 import Stack from '@mui/material/Stack';
 
-//#region Icon and label
-export const IconAndLabelExample = () => {
-  const [target, setTarget] = React.useState('');
+//#region Icon & label button
+export const IconAndLabelButton: React.FunctionComponent = () => {
   return (
-    <Stack direction='row' spacing={10}>
-      <IconButton
-        color='secondary'
-        aria-label='add an alarm'
-        data-testid='icon-button'
-        onClick={() => setTarget('icon-button')}>
-        <AlarmIcon />
-      </IconButton>
-      <Button
-        variant='contained'
-        endIcon={<SendIcon />}
-        data-testid='icon-label-button'
-        onClick={() => setTarget('icon-label-button')}>
+    <Stack direction='row' spacing={2}>
+      <Button data-testid='icon-button' variant='outlined' startIcon={<DeleteIcon />}>
+        Delete
+      </Button>
+      <Button data-testid='icon-label-button' variant='contained' endIcon={<SendIcon />}>
         Send
       </Button>
-      <div data-testid='target'>{target}</div>
     </Stack>
   );
 };
 
-export const iconAndLabelExampleScenePart = {
-  iconButton: {
-    locator: byDataTestId('icon-button'),
-    driver: ButtonDriver,
-  },
-  iconLabelButton: {
-    locator: byDataTestId('icon-label-button'),
-    driver: ButtonDriver,
-  },
-  target: {
-    locator: byDataTestId('target'),
-    driver: HTMLElementDriver,
-  },
-} satisfies ScenePart;
-
 /**
- * Icon button example from MUI's website
- * @see https://mui.com/material-ui/react-button/#icon-button
+ * Icon and label button example from MUI's website
+ * @see https://mui.com/material-ui/react-button#icon-and-label-buttons
  */
-export const iconAndLabelExample: IExampleUnit<typeof iconAndLabelExampleScenePart, JSX.Element> = {
+export const iconAndLabelButtonUIExample: IExampleUIUnit<JSX.Element> = {
   title: 'Icon & Label',
-  scene: iconAndLabelExampleScenePart,
-  ui: <IconAndLabelExample />,
+  ui: <IconAndLabelButton />,
 };
 //#endregion
-
-export const iconAndLabelButtonTestSuite: TestSuiteInfo<typeof iconAndLabelExampleScenePart> = {
-  title: 'Icon & Label',
-  url: '/button',
-  tests: (getTestEngine, { describe, test, beforeEach, afterEach, assertEqual }) => {
-    describe(`${iconAndLabelExample.title}`, () => {
-      let testEngine: TestEngine<typeof iconAndLabelExample.scene>;
-
-      // Use the following beforeEach to work around the issue of Playwright's page being undefined
-      // @ts-ignore
-      beforeEach(function ({ page }) {
-        // @ts-ignore
-        testEngine = getTestEngine(iconAndLabelExample.scene, { page });
-        if (typeof arguments[0] === 'function') {
-          arguments[0]();
-        }
-      });
-
-      afterEach(async () => {
-        await testEngine.cleanUp();
-      });
-
-      test(`Target should be empty initially`, async () => {
-        const text = await testEngine.parts.target.getText();
-        assertEqual(text, '');
-      });
-
-      test(`Click on icon-button should display icon-button`, async () => {
-        await testEngine.parts.iconButton.click();
-        const text = await testEngine.parts.target.getText();
-        assertEqual(text, 'icon-button');
-      });
-
-      test(`Click on icon-label-button should display icon-label-button`, async () => {
-        await testEngine.parts.iconLabelButton.click();
-        const text = await testEngine.parts.target.getText();
-        assertEqual(text, 'icon-label-button');
-      });
-    });
-  },
-};
