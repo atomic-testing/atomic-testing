3adf9dfeeab4291d5d2a4a7bad898bf94c266f1c
Tianzhen Lin (Tangent) <tangent@usa.net>
2025-06-17 22:21:52 -0400

    refactor: Split React package into core and react-18
    This pull request introduces a major breaking change:
    
    1. Splits `@atomic-testing/react` into `@atomic-testing/react-legacy` and `@atomic-testing/react-18` for better code separation
    2. Remove `@atomic-testing/react` package
    3. Create a share `@atomic-testing/core` with code shared across different versions of react testing engines.

--- packages/react/src/index.ts
+++
@@ -1,8 +0,0 @@
-export { createLegacyTestEngine, createRenderedLegacyTestEngine } from './createLegacyTestEngine';
-export { createRenderedTestEngine, createTestEngine } from './createTestEngine';
-export type { IReactTestEngineOption } from './types';
-
-console.warn(
-  '@atomic-testing/react → This package has been renamed to @atomic-testing/react-18. ' +
-    'Please update your package.json to depend on @atomic-testing/react-18 directly.'
-);
