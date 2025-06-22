851b7b8796032237535fb160a52cbb5031d01009
Tianzhen Lin (Tangent) <tangent@usa.net>
2025-06-21 23:29:18 -0400

    feat(core): export ItemNotFoundError

--- packages/core/src/errors/index.ts
+++ packages/core/src/errors/index.ts
@@ -1,4 +1,5 @@
 export { ErrorBase } from './ErrorBase';
+export { ItemNotFoundError, ItemNotFoundErrorId } from './ItemNotFoundError';
 export { MissingPartError, MissingPartErrorId } from './MissingPartError';
 export { TooManyMatchingElementError, TooManyMatchingElementErrorId } from './TooManyMatchingElementError';
 export { WaitForFailureError, WaitForFailureErrorId } from './WaitForFailureError';
