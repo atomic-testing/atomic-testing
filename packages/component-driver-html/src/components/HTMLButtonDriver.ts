7adfa1df42cb639cfaa1d3cab16541cca41cd2f6
Tianzhen Lin (Tangent) <tangent@usa.net>
2025-06-21 23:18:31 -0400

    docs: add typedoc comments for HTML drivers

--- packages/component-driver-html/src/components/HTMLButtonDriver.ts
+++ packages/component-driver-html/src/components/HTMLButtonDriver.ts
@@ -1,10 +1,19 @@
 import { ComponentDriver, IClickableDriver, IMouseInteractableDriver } from '@atomic-testing/core';
 
 export class HTMLButtonDriver extends ComponentDriver<{}> implements IClickableDriver, IMouseInteractableDriver {
+  /**
+   * Check if the button element is disabled.
+   *
+   * @returns A promise that resolves to `true` when the underlying
+   * `disabled` attribute is present.
+   */
   isDisabled(): Promise<boolean> {
     return this.interactor.isDisabled(this.locator);
   }
 
+  /**
+   * Identifier for this driver. Primarily used by debugging utilities.
+   */
   get driverName(): string {
     return 'HTMLButtonDriver';
   }
--- packages/component-driver-html/src/components/HTMLElementDriver.ts
+++ packages/component-driver-html/src/components/HTMLElementDriver.ts
@@ -4,6 +4,9 @@ import { ComponentDriver } from '@atomic-testing/core';
  * Generic HTML element driver for non-interactive elements
  */
 export class HTMLElementDriver extends ComponentDriver<{}> {
+  /**
+   * Identifier for this driver.
+   */
   get driverName(): string {
     return 'HTMLElementDriver';
   }
