497a2d54da88d3f1220cfdf93578734cb0b246ca
tangent.lin@dthxtkx5mv.tail21382.ts.net <>
2025-06-28 16:18:09 -0400

    refactor: Update code style
    Summary:
    
    Test Plan:

--- package-tests/component-driver-mui-v6-test/__tests__/CheckboxDriver.dom.test.ts
+++ package-tests/component-driver-mui-v6-test/__tests__/CheckboxDriver.dom.test.ts
@@ -1,6 +1,6 @@
+import { testRunner } from '@atomic-testing/internal-test-runner';
 import { jestTestAdapter } from '@atomic-testing/internal-test-runner-jest-adapter';
 import { createTestEngine } from '@atomic-testing/react-18';
-import { testRunner } from '@atomic-testing/internal-test-runner';
 
 import {
   iconCheckboxExample,
