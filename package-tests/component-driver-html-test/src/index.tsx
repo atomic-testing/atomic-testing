7428fdde9effd6107bad2ef1c251e373b1229e6f
Tangent Lin <>
2025-12-30 18:55:24 -0500

    chore: update React types and fix JSX namespace imports on test packages
    - Align @types/react and @types/react-dom  in MUI test packages (v5, v6, v7, x-v5, x-v6, x-v7)
    - Add explicit `import { JSX } from 'react'` statements to example and suite files where JSX types are used
    - Add check:type script to package.json files for type-checking validation
    - Update partTypes.ts with improved type definitions

--- package-tests/component-driver-html-test/src/index.tsx
+++ package-tests/component-driver-html-test/src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
-import { ExampleApp } from '@atomic-testing/internal-react-example';
+import { ExampleApp } from '@atomic-testing/internal-react-example-19';
 
 import { Home } from './Home';
 import { tocs } from './directory';
